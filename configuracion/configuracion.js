95844b482a21571b8e4d684689fc79a070893f7d
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2021-09-23 00:06:25 -0300

    Primera subida a github

---
+++ configuracion/configuracion.js
@@ -0,0 +1,35 @@
+const SeqLibrary = require("sequelize");
+
+const conn =
+
+{
+    DATABASE: 'datawarehouse',
+    DIALECT: 'mysql',
+    HOST: '127.0.0.1',
+  // PASSWORD: 'Acamica123',
+    USERNAME: 'root'
+}
+
+
+const sequelize = new SeqLibrary(conn.DATABASE, conn.USERNAME, conn.PASSWORD, {
+    host: conn.HOST,
+    dialect: conn.DIALECT,
+});
+
+sequelize.authenticate().then(() => {
+    console.log("Conectado a la base de datos");
+}).catch(err => {
+    console.error("Error de conexion" + err);
+})/*.finally( ()=> {
+    sequelize.close();
+})*/;
+
+const jwt = require('jsonwebtoken');
+const firma = "LaPalabraMasica2020";
+const puerto = 3001;
+
+const m = new Date();
+const ahora = m.getUTCFullYear() +"/"+ (m.getUTCMonth()+1) +"/"+ m.getUTCDate() + " " + m.getUTCHours() + ":" + m.getUTCMinutes() + ":" + m.getUTCSeconds();
+
+
+module.exports = { sequelize, jwt, firma, ahora, puerto };
